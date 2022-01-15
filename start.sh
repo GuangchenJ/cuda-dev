aebd3f31f1dcdd922ed2ee99f47eabe03a297e55
Guangchen Jiang <guangchen98.jiang@gmail.com>
2022-01-15 13:03:43 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # Set the new password
-export pwd=new_password
+echo "Enter the new password of the user root in the container"
+read new_pwd
+export pwd=$new_pwd
 
 # build Docker Image
 docker-compose build
 
 docker-compose up -d
-
